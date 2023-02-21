33652947d96b100cabb3f4fb04dce02c2c637e4e
Adele2007 <adelyaalmazova@icloud.com>
2023-02-21 18:27:08 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework_1_3_m/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.homework_1_3_m;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.txt_view);
+        button = findViewById(R.id.button_send);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,textView.getText().toString());
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+    }
+}
